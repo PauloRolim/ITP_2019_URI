c0290085769f72f758d740abe5efdce389f2c7a0
Paulo Rolim <41303759+PauloRolim@users.noreply.github.com>
2019-09-07 20:43:58 -0300

    Create 1059.c

---
+++ 1059.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+
+    int i, num;
+
+    for (i = 1; i <= 100; i++){
+        num = i % 2;
+
+        if (num % 2 == 0){
+            printf("%d\n", i);
+        }
+
+        if (i == 100) {
+            break;
+        }
+    }
+    
+    return 0;
+}
